2e140ed9cbbf877bc8e9ba85f0c337d56a49e879
gpl4010 <gpl4010@gmail.com>
2024-04-02 16:38:35 +0900

    front-end 구성작업 1차

---
+++ Frontend/rename.bat
@@ -0,0 +1,17 @@
+@echo off
+
+ren "blank - 복사본 (2).html" InfoTechWrLearn.html
+ren "blank - 복사본 (3).html" InfoTechPrLearn.html
+ren "blank - 복사본 (4).html" InfoTechWrTest.html
+ren "blank - 복사본 (5).html" InfoTechPrTest.html
+ren "blank - 복사본 (6).html" InfoIEngineerWrLearn.html
+ren "blank - 복사본 (7).html" InfoIEngineerPrLearn.html
+ren "blank - 복사본 (8).html" InfoIEngineerWrTest.html
+ren "blank - 복사본 (9).html" InfoIEngineerPrTest.html
+ren "blank - 복사본 (10).html" InfoEngineerWrLearn.html
+ren "blank - 복사본 (11).html" InfoEngineerPrLearn.html
+ren "blank - 복사본 (12).html" InfoEngineerWrTest.html
+ren "blank - 복사본 (13).html" InfoEngineerPrTest.html
+ren "blank - 복사본 (14).html" Contact.html
+
+pause
